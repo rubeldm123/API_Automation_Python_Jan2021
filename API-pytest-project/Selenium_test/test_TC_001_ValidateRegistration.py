6badd36698b181700b96d49af323a32fe84de59b
rubeldm123 <rubeldm123@gmail.com>
2021-01-23 17:43:58 -0600

    Modified Testcase and Chromedriver path

--- API-pytest-project/Selenium_test/test_TC_001_ValidateRegistration.py
+++ API-pytest-project/Selenium_test/test_TC_001_ValidateRegistration.py
@@ -2,7 +2,9 @@ import pytest
 from selenium.webdriver import Chrome
 @pytest.mark.Smoke
 def test_registration_valid_data():
-    path= "/Selenium_test/chromedriver"
+    global path
+    path= "../Selenium_test/chromedriver"
+
     driver=Chrome(executable_path=path)
     driver.get("https://opensource-demo.orangehrmlive.com/")
     driver.maximize_window()
@@ -10,9 +12,6 @@ def test_registration_valid_data():
 
 @pytest.mark.Sanity
 def test_registration_invalid1_data():
-    global path
-    path= "/Users/mdrubel/Downloads/python_workspace/API-pytest-project/Selenium_test/chromedriver"
-
     driver=Chrome(executable_path=path)
     driver.get("https://opensource-demo.orangehrmlive.com/")
     driver.maximize_window()
